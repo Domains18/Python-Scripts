abd9ae64fe03bd5891c3a8d747a0c3b3a0cf47cc
Domains18 <gibsonsgibson88@gmail.com>
2023-01-12 08:05:40 +0300

    changing windows mac address

--- change-windows-mac-address.py
+++ change-windows-mac-address.py
@@ -11,3 +11,18 @@ macAddresses = list()
 macAddRegex = re.compile(r"([A-Za-z0-9]{2}[:-]){5}([A-Za-z0-9]{2})")
 transportName = re.compile("({.+})")
 adapterIndex = re.compile("([0-9]+)")
+getMacOutput = subprocess.run("getmac", capture_output=True).stdout.decode().split("\n")
+for macAdd in getMacOutput:
+    macFind = macAddRegex.search(macAdd)
+    transportFind = transportName.search(macAdd)
+    if macFind == None or transportFind == None:
+        continue
+macAddresses.append((macFind.group(0), transportFind.group(0)))
+print("Select MAc address to update")
+for index, item in enumerate(macAddresses):
+    print(f"{index} - Mac Address: {item[0]} - transport name: {item[1]}")
+    
+option = input("Select The Menu item corresponding to the MAC that you want to change: ")
+
+while True:
+    print(W)        
