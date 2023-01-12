c85ace9adbbd19a43dfea398e21017e27db29397
Domains18 <gibsonsgibson88@gmail.com>
2023-01-12 07:47:05 +0300

    macChanger

--- change-windows-mac-address.py
+++ change-windows-mac-address.py
@@ -1,2 +1,13 @@
 import subprocess
 import winreg
+import re
+import codecs
+
+print("Run With Administrator privileges")
+print("To Whom Much is Given, Much Is Expected")
+
+macToChangeTo = ["0A1122334455", "0E1122334455", "021122334455", "061122334455"]
+macAddresses = list()
+macAddRegex = re.compile(r"([A-Za-z0-9]{2}[:-]){5}([A-Za-z0-9]{2})")
+transportName = re.compile("({.+})")
+adapterIndex = re.compile("([0-9]+)")
