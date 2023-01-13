91021ddcfd6f8c3670f944071c800d59a4c4c16b
Domains18 <gibsonsgibson88@gmail.com>
2023-01-13 12:19:03 +0300

    exif csv

--- exif_csv/exifCsv.py
+++ exif_csv/exifCsv.py
@@ -14,3 +14,30 @@ def convertToDecDeg(deg, min, sec, ref):
         decDeg *= -1
     return decDeg
 
+cwd = os.getcwd()
+os.chdir(os.path.join(cwd, "images"))
+files = os.listdir()
+
+if len(files) == 0:
+    print("No images found in images folder")
+    exit()
+    
+with open("exif.csv", "w", newline="") as csvFile:
+    csvWriter = csv.writer(csvFile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    csvWriter.writerow(["File Name", "Date Taken", "Latitude", "Longitude", "Google Maps URL"])
+    for file in files:
+        try:
+            image = Image.open(file)
+            exifData = image._getexif()
+            gpsData = {}
+            for tagId, value in exifData.items():
+                tag = TAGS.get(tagId, tagId)
+                if tag == "GPSInfo":
+                    for t in value:
+                        subTag = GPSTAGS.get(t, t)
+                        gpsData[subTag] = value[t]
+            csvWriter.writerow([file, exifData[36867], gpsData["GPSLatitude"], gpsData["GPSLongitude"], createGooglrMapUrl(gpsData)])
+        except:
+            print(f"Error reading exif data from {file}")
+            
+os.chdir(cwd)
